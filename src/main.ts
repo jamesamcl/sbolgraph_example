b8f7ec71f2b921e1bba017259c6c56b18501dba8
James Alastair McLaughlin <james@mclgh.net>
2018-11-09 20:00:58 +0000

    change module setting to commonjs

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function main() {
     let url:string = 'https://synbiohub.org/public/igem/BBa_K104001/1/BBa_K104001.xml'
 
     console.log('Loading SBOL from ' + url)
-    let g = await SBOL2Graph.loadURL(url)
-
+    let g:SBOL2Graph = await SBOL2Graph.loadURL(url)
+    
     for(let cd of g.componentDefinitions) {
 
         document.write('There is a ComponentDefinition called ' + cd.displayName + '<br/>')
